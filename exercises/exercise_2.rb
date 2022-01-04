f98d2712e1a565ae44bee82c4ba25aa5c4cb23a0
Kenneth Chen <kenchen0606@gmail.com>
2022-01-04 05:41:14 +0000

    complete 1 to 4

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+store1 = Store.find_by(id = @store1)
+
+store1.name = 'DaveTown'
+store1.save
+
+store2 = Store.find_by(id = @store2)
+store2.update(name: 'SunnyBoi')
